036f14da4ce5e36f1583699f8e0804f53684210d
Claudinei Machado <claudinei@claudinei.KolinaLabs>
2019-05-08 20:07:38 -0300

    Handle not found

--- stack/read.js
+++ stack/read.js
@@ -4,6 +4,18 @@
  * @param {*} next
  */
 module.exports = async (req, res, next) => {
+  /**
+   * @todo Add skip process option here
+   * suggest: req.ApiPack.receive = false
+   */
+
   await req.ApiPack.read();
+
+  if (!req.ApiPack.operation.data && req.method !== "POST") {
+    return res.status(404).send({
+      message: "Not Found"
+    });
+  }
+
   next();
 };
